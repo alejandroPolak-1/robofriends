b2c095fe622c2df805e6188c7809328b5c7b1982
Alejandro Daniel Polak <alejandro_polak@hotmail.com>
2019-11-26 09:14:05 -0300

    rev

---
+++ build/precache-manifest.4a5bf59f24fc902c29b57890558fb7f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcc7cbf16848cf7d7928d7ffd238dc0",
+    "url": "alejandroPolak-1.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "alejandroPolak-1.github.io/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0a22066940a936156ce5",
+    "url": "alejandroPolak-1.github.io/robofriends/static/css/main.46a49b62.chunk.css"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "alejandroPolak-1.github.io/robofriends/static/js/2.7514f082.chunk.js"
+  },
+  {
+    "revision": "0a22066940a936156ce5",
+    "url": "alejandroPolak-1.github.io/robofriends/static/js/main.a44b185c.chunk.js"
+  },
+  {
+    "revision": "1a03e96f229b18f56bcb",
+    "url": "alejandroPolak-1.github.io/robofriends/static/js/runtime-main.3026a8c7.js"
+  }
+]);
--- src/conteiners/App.js
+++ src/conteiners/App.js
@@ -1,37 +1,47 @@
 import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import { robots } from '../components/robots';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import CardList from './components/CardList';
+// import { robots } from './components/robots';
+import SearchBox from './components/SearchBox';
+import Scroll from './components/Scroll'
 
 class App extends Component {
     constructor() {
         super()             //llama al constructor
         this.state = {
-            robots: robots,   //ahora robots es parte del objeto, de la propiedad state
+            robots: [],   //se crea, y luego que los datos dl servidor se monten lo garga
             searchfield: '',
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())   //json(), transforma lo que recibe en archivo json
+        .then(users => this.setState({ robots:users })); 
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
     
     render () {
-        const { robots, searchfield } = this.state;   
-        const filteredRobots= robots.filter(robot=> {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        const filteredRobots= this.state.robots.filter(robot=> {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-       return !robots.length 
-        ?  <h1> Cargando ... </h1> 
-        : (<div className='tc'>
-                     <h1 style= {{color: 'black'}}>RoboFriends</h1>
-                     <SearchBox searchChange={this.onSearchChange}/>
-                     <Scroll>
-                        <CardList robots={filteredRobots}/>   
-                     </Scroll>
-          </div>);
-      }   
- }
+        if(this.state.robots.length === 0) {
+            return <h1> Loaring ... </h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 style= {{color: 'black'}}>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                       <CardList robots={filteredRobots}/>   
+                    </Scroll>
+                </div>
+          );
+      }
+   }
+}
 
 export default App;
 
