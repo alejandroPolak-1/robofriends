8fff519d4f6bb4f9930a3ed04c7b8ba26e5dee4a
Alejandro Daniel Polak <alejandro_polak@hotmail.com>
2019-11-28 13:21:21 -0300

    agregado Redux

--- src/conteiners/App.js
+++ src/conteiners/App.js
@@ -1,50 +1,70 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
-// import { robots } from './components/robots';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import './App.css'
 
-class App extends Component {
-    constructor() {
-        super()             //llama al constructor
-        this.state = {
-            robots: [],   //se crea, y luego que los datos dl servidor se monten lo garga
-            searchfield: '',
-        }
+import { setSearchField, requestRobots } from '../actions'
+
+// creados para REDUX
+const mapStateToProps = state => {
+    return {   
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.IsPending,
+        error: state.requestRobots.error
     }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+
+    }
+}
+
+// 
+
+class App extends Component {
+    // constructor() {
+    //     super()             //llama al constructor
+    //     this.state = {
+    //         robots: []   //se crea, y luego que los datos dl servidor se monten lo garga
+           
+    //     }
+    // }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())   //json(), transforma lo que recibe en archivo json
-        .then(users => this.setState({ robots:users })); 
+      this.props.onRequestRobots();
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    // onSearchChange = (event) => {
+    //     this.setState({ searchfield: event.target.value })
+    // }
     
     render () {
-        const filteredRobots= this.state.robots.filter(robot=> {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filteredRobots= robots.filter(robot=> {
+            return robot.name.toLowerCase().includes(searchField);
         })
-        if(this.state.robots.length === 0) {
-            return <h1> Loaring ... </h1>
-        } else {
-            return (
+        return isPending ?
+            <h1> Loaring ... </h1> :
+             (
                 <div className='tc'>
                     <h1 className='f1' >RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                        <CardList robots={filteredRobots}/>   
                     </Scroll>
                 </div>
           );
-      }
+    //   }
    }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 
     
